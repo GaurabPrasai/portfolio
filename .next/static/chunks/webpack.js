b2f81c7a0bbb779cac66fe708e14d29116c070c6
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2025-12-14 21:28:08 +0545

    add icon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "512bbcc4cf91a540"; }
+/******/ 		__webpack_require__.h = function() { return "df6c3576ebd21db9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
